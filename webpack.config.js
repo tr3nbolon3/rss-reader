45d877f07195ebcdc6987a9dd0c16fb628b0db64
Kostik <zemliakov.konstantin@gmail.com>
2018-07-24 11:47:45 +0600

    add serve config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const dist = path.join(__dirname, '/dist');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    path: `{${__dirname}/dist}`,
+    path: dist,
     filename: 'bundle.js',
   },
   module: {
@@ -28,4 +31,8 @@ module.exports = {
       template: 'template.html',
     }),
   ],
+  serve: {
+    content: dist,
+    open: true,
+  },
 };
